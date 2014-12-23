ad010961a9d4305d44f6aa418fc45cfdcd8a9596
pattyjubu <one.two.jungo@gmail.com>
2014-12-24 06:35:13 +0700

    edit

--- myserver/public/js/app.js
+++ myserver/public/js/app.js
@@ -5,6 +5,31 @@ angular.module("myApp",['btford.socket-io'])
   });
 
 })
+.controller('mainCtrl', function($scope,$http,socketIO){
+   $scope.books = [];
+  $scope.bookInstance = {};
+  $scope.std = [];
+
+  refleshBooks();
+
+function refleshBooks(){    
+    $http.get('/api/savetime').success(function(data){
+      $scope.std = data;
+      console.log(data);
+    })
+}
+  $scope.save =function(){
+    $http.post('/api/book',$scope.bookInstance).success(function(data){
+      $scope.books.push(data);
+      $scope.bookInstance={};
+    });
+  }
+
+  socketIO.on('book:reflesh',function(){
+    refleshBooks();
+  });
+
+})
 
 .controller('adminCtrl', function($scope,$http){
   $scope.persons = [];
